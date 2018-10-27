dc67de00c425f7ad6f8538b3a82e2c63da82f1d8
theflow <theflow@debian.theflow.net>
2018-10-27 20:49:03 +0200

    Travail initial sur les bits

--- fonctions.c
+++ fonctions.c
@@ -21,24 +21,115 @@ void INIT_LAB(LABYRINTHE * LAB, int nbltmp, int nbcoltmp){
 	LAB->psortieY=9;
 	LAB->posXChercheur=LAB->pentreeX;
 	LAB->posYChercheur=LAB->pentreeY;
-}
-
-void AFFICHE_LAB(LABYRINTHE *LAB){
-	int i,j;
-	printf("────────────────────────────────────────\n");
-	for(i=0; i<LAB->nbl; i++){
-		for(j=0; j<LAB->nbcol; j++){
-			printf("▏%2hu ", LAB->lab[i][j]);
-		}
-		printf("▏\n");
-		printf("────────────────────────────────────────\n");
-	}
-}
+}/*
 void INIT_ALEA_LAB(LABYRINTHE *LAB){
 	int i,j;
 	for(i=0; i<LAB->nbl; i++){
 		for(j=0; j<LAB->nbcol; j++){
-			LAB->lab[i][j]=(unsigned short)(rand()%15)+1;
+			if(i==0&&j==0){
+				int liste[]={11,13};
+				int r =rand()%2;
+				LAB->lab[i][j]=(unsigned short)liste[r];
+			}
+			else if(i==0){
+				if(((LAB->lab[i][j-1]>>3)&1)==1){
+					LAB->lab[i][j]=(unsigned short)(12+rand()%4);
+				}
+				else{
+					int liste1 []={8,10,12,14};
+					int r1=rand()%4;
+					LAB->lab[i][j]=(unsigned short)(liste1[r1]);
+				}
+			}
+			else if(i==0&&j==LAB->nbcol-1){
+				if(((LAB->lab[i][j-1]>>3)&1)==1){
+					int liste2 []={13, 15};
+					int r2 =rand()%2;
+					LAB->lab[i][j]=(unsigned short)liste2[r2];
+				}
+				else{
+					int liste3 []={12,14};
+					int r3=rand()%2;
+					LAB->lab[i][j]=(unsigned short)(liste[r3]);
+				}
+			}
+			else if(j==0){
+				if(((LAB->lab[i-1][j]>>1)&1)==1){
+					int liste4 []={11, 13, 15};
+					int r4=rand()%3;
+					LAB->lab[i][j]=(unsigned short)(liste[r4]);
+				}
+				else{
+					int liste5 []={1,3,5,7};
+					int r5=rand()%4;
+					LAB->lab[i][j]=(unsigned short)(liste[r5]);
+				}
+			}
+			else if(i==LAB->nbl-1){
+
+			}
+			}
+			else if(j==LAB->nbcol-1){
+				
+			}
+			else{
+				if(((LAB->lab[i-1][j]>>1)&1)==1){
+					int liste4 []={11, 13, 15};
+					int r4=rand()%3;
+					LAB->lab[i][j]=(unsigned short)(liste[r4]);
+				}
+				else{
+					int liste5 []={1,3,5,7};
+					int r5=rand()%4;
+					LAB->lab[i][j]=(unsigned short)(liste[r5]);
+				}
+				
+
+			}
+
+		}
+//Initialistation cellule 0-15
+1111 : tous les murs ->15 
+1000 : mur haut -> 8
+0100 : mur droite->4
+0010 : mur bas ->2
+0001 : mur gauche ->1
+
+1001
+}*/
+void INIT_ALEA_LAB(LABYRINTHE *LAB){
+	int i,j;
+
+	for(i=0; i<LAB->nbl; i++){
+		for(j=0; j<LAB->nbcol; j++){
+			if(i==0&&j==0){
+				unsigned short liste[]={2,4};
+				int r =rand()%2;
+				LAB->lab[i][j]=(unsigned short)(LAB->lab[i][j]^liste[r]);
+			}
+			else if(i==0){
+				unsigned short predroite = (LAB->lab[i][j-1]>>2)&1;
+				unsigned short cegauche = 8^predroite;
+				LAB->lab[i][j]=(unsigned short)(cegauche+rand()%7);
+
+			}
+			else if(i==0&&j==LAB->nbcol-1){
+				unsigned short predroite1 = (LAB->lab[i][j-1]>>2)&1;
+				unsigned short cegauche1 = 12^predroite1;
+				LAB->lab[i][j]=(unsigned short)(cegauche1+rand()%4);
+			}
+			else if(j==0){
+				unsigned short prebas = (LAB->lab[i-1][j]>>1)&1;
+				unsigned short cehaut = 1^(prebas<<3);
+				LAB->lab[i][j]=(unsigned short)(cehaut+rand()%7);
+				
+			}
+			else{
+				unsigned short liste1[]={4, 2};
+				unsigned short prebas1 = (LAB->lab[i-1][j]>>1)&1;
+				unsigned short predroite1 = (LAB->lab[i][j-1]>>2)&1;
+				LAB->lab[i][j]=LAB->lab[i][j]^prebas1^predroite1^liste1[(rand()%2)];
+			}
 		}
 	}
 }
@@ -73,109 +164,48 @@ void INIT_LAB_VIA_FIC(LABYRINTHE *LAB, FILE * FIC){
 		}
 	}
 }
-void AFFICHE_CASE_HAUT(LABYRINTHE *LAB){
-	int i,j;
-	for(i=0; i<LAB->nbl; i++){
-		for(j=0; j<LAB->nbcol; j++){
-			unsigned short haut =((LAB->lab[i][j]>>3)&1);
-			if(haut == 1){
-			printf("────");
-			}
-		}
-		printf("\n");
-		printf("\n");
-	}
-}
-void AFFICHE_CASE_GAUCHE(LABYRINTHE *LAB){
-	char barre[] ="▏";
-	int indice = 0;
-	int indice1 = 2;
-	int i,j;
-	for(i=0; i<LAB->nbl; i++){
-		for(j=0; j<LAB->nbcol; j++){
-			unsigned short gauche=(LAB->lab[i][j]&1);
-			printf("\033[%d;%dH",indice1, indice);
-			if(gauche == 1){
-				printf("%s", barre);
-			}
-			indice+=4;
-		}
-		indice1 +=2;
-		indice=0;
-	}
-}
-void AFFICHE_CASE_DROITE(LABYRINTHE *LAB){
-	char barre[] ="▏";
-	int indice = 4;
-	int indice1 = 2;
-	int i,j;
-	for(i=0; i<LAB->nbl; i++){
-		for(j=0; j<LAB->nbcol; j++){
-			unsigned short right=((LAB->lab[0][0]>>2)&1);
-			printf("\033[%d;%dH",indice1, indice);
-			if(right== 1){
-				printf("%s", barre);
-			}
-			indice+=4;
-		}
-		indice1 +=2;
-		indice=0;
-	}
-}
-void AFFICHE_LAB2(LABYRINTHE *LAB){
+void AFFICHE_LAB(LABYRINTHE *LAB){
 	int i,j;
 	for(i=0; i<LAB->nbl; i++){
 		for(j=0; j<LAB->nbcol; j++){
 			if(i==0){
-				printf("────");
+				printf("+---");
 			}
 			else{
-				unsigned short bottom=(unsigned short)((LAB->lab[i][j]>>1)&1);
-				if(bottom==1){
-					printf(" ── ");
+				unsigned short top=(unsigned short)((LAB->lab[i][j]>>3)&1);
+				if(top==1){
+					printf("+---");
+				}
+				else{
+					printf("+   ");
 				}
 			}
 		}
-		printf("\n");
+		printf("+\n");
 		
 		for(j=0; j<LAB->nbcol; j++){
 			if(j==0){
-				printf("▏%2hu ", LAB->lab[i][j]);
+				printf("| %2hu", LAB->lab[i][j]);
 			}
 			else{
 				unsigned short gauche=(LAB->lab[i][j]&1);
-				if(gauche==1){
-					printf("▏%2hu ", LAB->lab[i][j]);
+				unsigned short droite=((LAB->lab[i][j]>>2)&1);
+				if(gauche==1||droite==1){
+					printf("| %2hu", LAB->lab[i][j]);
 				}
 				else{
-					printf(" %2hu ", LAB->lab[i][j]);
+					printf("  %2hu", LAB->lab[i][j]);
 				}
 			}
 		}
-		printf("▏\n");
+		printf("|\n");
+	
 		for(j=0; j<LAB->nbcol; j++){
 			if(i==LAB->nbl-1){
-				printf("────");
+			printf("+---");
 			}
 		}
 	}
+	printf("+\n");
 }
-/*
-		printf("▏  ");
-	}
-	if(right==1){
-		printf("▏");
-	}
-	if(bottom==1){
-		printf("\n───");
-	}*/
-//Initialistation cellule 0-15
-/*
-1111 : tous les murs ->15 
-1000 : mur haut -> 8
-0100 : mur droite->4
-0010 : mur bas ->2
-0001 : mur gauche ->1
-		
- */	  
 
--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[]){
 	srand(time(NULL));
 	LABYRINTHE *LAB =(LABYRINTHE *)malloc(sizeof(LABYRINTHE));
-	INIT_LAB(LAB, 20, 20);
+	INIT_LAB(LAB, 4,4);
 	//AFFICHE_LAB(LAB);
 	//printf("TABLEAU ALEATOIRE :\n");
-//	INIT_ALEA_LAB(LAB);
+	INIT_ALEA_LAB(LAB);
 	//AFFICHE_LAB(LAB);
 	//FILE * FIC = fopen("Labyrinthe.txt","w+");
 	//ECRIT_LAB_DS_FIC(LAB, FIC);
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]){
 	//INIT_LAB_VIA_FIC(LAB, FIC);
 	system("clear");
 	printf("\033[1;1H");
-	AFFICHE_LAB2(LAB);
+	AFFICHE_LAB(LAB);
 	//AFFICHE_CASE_HAUT(LAB);
 	//AFFICHE_CASE_GAUCHE(LAB);
 	//AFFICHE_CASE_DROITE(LAB);
